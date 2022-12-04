95e37ba3783956018b05be1ee17465f6c3697270
Vlad32032 <111176660+Vlad32032@users.noreply.github.com>
2022-12-04 13:43:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,37 +1,65 @@
-// Нода для вставки результата запроса
+// нода для вставки результата запроса
 const resultNode = document.querySelector(".result");
-// Нода для кнопки
+// нода для кнопки
 const button = document.querySelector(".button");
+// переменная для проверки значения из localStorage
+let myJSON = localStorage.getItem('myJSON');
 
-// Функция -обертка над XMLHttpRequest 
-// возвращает объект из url и вызывает callback-функцию 
-function useRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+// вызываем функию при загрузке страницы, которая проверяет localStorage и выводит блоки с картинками
+onload(myJSON); 
 
-    xhr.onload = function() {
-        if (xhr.status != 200) {
-            console.log(`Статус ответа: ${xhr.status}`)
-        } else {
-            const result = JSON.parse(xhr.response);
-            if (callback) {
-                callback(result);
-            }
-        }
-    };
+// создаем обработчик для кнопки
+button.addEventListener("click", () => {
+    // сохраняем в переменные значения из input
+    let userValue1 = document.querySelector("#num_input").value;
+    let userValue2 = document.querySelector("#num_input2").value;
 
-    xhr.onerror = function() {
-        console.log(`Ошибка! Статус ответа: ${xhr.status}`)
-    };
+    // проверяем уловия ввода
+    if ((userValue1 >= 1 && userValue1 <= 10) && (userValue2 >= 1 && userValue2 <= 10)) {
+        fetch(`https://picsum.photos/v2/list?page=${userValue1}&limit=${userValue2}`)
+            .then((response) => {return response.json();})
+            .then((data) => {
+                // сохраняем в localStorage полученный JSON в виде строки
+                localStorage.setItem('myJSON', JSON.stringify(data));
+                // вызываем функцию displayResult, которая добавляет код в HTML
+                displayResult(data);
+            })
+            .catch(() => {
+                // выводим в консоль сообщение
+                console.log('error')
+            });
+        // скрываем запись про ошибку, если вдруг она до этого выводилась
+        document.querySelector(".error").style.opacity = "0";
+    //проверяем правильность первого значения
+    }  else if ((userValue1 < 1 || userValue1 > 10) && (userValue2 >= 1 || userValue2 <= 10)) {
+        // добавляем ошибку и меням текст ошибки 
+        document.querySelector(".error").style.opacity = "1";
+        document.querySelector(".error").innerHTML = "Номер страницы вне диапазона от 1 до 10";
+    //проверяем правильность второго значения
+    } else if ((userValue1 >= 1 || userValue1 <= 10) && (userValue2 < 1 || userValue2 > 10)) {
+        // добавляем ошибку и меням текст ошибки 
+        document.querySelector(".error").style.opacity = "1";
+        document.querySelector(".error").innerHTML = "Лимит вне диапазона от 1 до 10";
+    } else {
+        // добавляем ошибку и меням текст ошибки 
+        document.querySelector(".error").style.opacity = "1";
+        document.querySelector(".error").innerHTML = "Номер страницы и лимит вне диапазона от 1 до 10";
+    }
+});
 
-    xhr.send();
-};
+// объявляем функцию, которая проверяет localStorage и создает HTML код
+function onload (myJSON) {
+    if (myJSON !== null) {
+        // преобразуем строку JSON в объект и передаем ее аргументом функции
+        displayResult(JSON.parse(myJSON));
+    }
+}
 
-// функция перебирает apiData, и выводит на экран новые карточки (добавляет html)
-// apiData - результат запроса(объект) из успешного выполения функции useRequest
+// объявляем функцию которая создает карточки с картинками для HTML
 function displayResult(apiData) {
+    // переменная для HTML кода
     let cards = "";
-
+    // с помощью метода .forEach проходим по каждому элементу массива, получем Url картинок, добовляем их в HTML код и плюсуем в переменную cards
     apiData.forEach(element => {
         let cardBlok = `
             <div class="card">
@@ -39,25 +67,10 @@ function displayResult(apiData) {
                     src="${element.download_url}"
                     class="card_img"
                 >
-                <span>${element.author}</span>
             </div>
         `;
         cards = cards + cardBlok;
     });
-
+    // добавляем сформированный HTML код в содержимое тега с class="result"
     resultNode.innerHTML = cards;
 };
-
-// обработчки для кнопки
-button.addEventListener("click", () => {
-    let userValue = +document.querySelector(".input").value;
-    // console.log(userValue)
-    if (userValue >= 1 && userValue <= 10) {
-        useRequest(`https://picsum.photos/v2/list/?limit=${userValue}`, displayResult);
-    } else {
-        document.querySelector(".error").style.opacity = "1";
-    }
-});
-
-
-
